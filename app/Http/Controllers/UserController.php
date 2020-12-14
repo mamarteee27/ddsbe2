c6eff143f30c202a47fb21eaee8cdacaf59ff612
unknown <jaguiman27@gmail.com>
2020-12-14 15:50:34 +0800

    Semi Final

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;  
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;  
+use Illuminate\Http\Request;  
+use DB; 
+
+
+Class UserController extends Controller {
+    use ApiResponser;
+
+    private $request;
+
+    public function __construct(Request $request){
+        $this->request = $request;
+    }
+
+    public function getUsers(){
+
+        $users = DB::connection('mysql')
+        ->select("Select * from tbluser");
+        return $this->successResponse($users);
+    }
+    
+    public function index()
+    {
+        $users = User::all();
+        return $this->successResponse($users);
+    }
+
+    public function addUser(Request $request ){
+        $rules = [
+            'username' => 'required|max:20',
+            'password' => 'required|max:20',
+            'jobid' => 'required|numeric|min:1|not_in:0',
+        ];
+
+        $this->validate($request,$rules);
+
+        // validate if Jobid is found in the table tbluserjob
+        $userjob = UserJob::findOrFail($request->jobid);
+
+        $user = User::create($request->all());
+
+        return $this->successResponse($user, Response::HTTP_CREATED);
+    }
+
+    
+    public function show($id)
+    {
+
+         $user = User::findOrFail($id);
+         return $this->successResponse($user);
+         
+       
+    }
+
+    
+    public function update(Request $request,$id)
+    {
+        $rules = [
+        'username' => 'max:20',
+        'password' => 'max:20',
+        'jobid' => 'required|numeric|min:1|not_in:0',
+        ];
+
+        $this->validate($request, $rules);
+
+        // validate if Jobid is found in the table tbluserjob
+        $userjob = UserJob::findOrFail($request->jobid);
+
+        $user = User::findOrFail($id);
+            
+        $user->fill($request->all());
+
+        //if no changes
+        if ($user->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user->save();
+        return $this->successResponse($user);
+       
+        
+    }
+
+    public function delete($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+
+        
+    }
+
+}
+
+
+
---
+++ app/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+    // change namespace to App\Models if you put your model inside models
+    namespace App\Models; 
+
+    // library to create Model under lumen
+    use Illuminate\Database\Eloquent\Model;
+
+    class User extends Model{
+        // The code below will not require the field create_at and update_at
+        
+        // name of table
+         protected $table = 'tbluser';
+        // column sa table
+         protected $fillable = [
+            'username', 'password', 'jobid'
+         ];
+
+        public $timestamps = false;
+        protected $primaryKey = 'id';
+    }
+
+    
